66977a908a61fe253c609dc92851a39d26b03653
RawHeatEcon <kumar.rohitlove@yahoo.com>
2022-11-20 21:05:15 -0600

    Headlines printing in new line on csv file*

--- NewsBot 2.0/NewsBot_2.0.py
+++ NewsBot 2.0/NewsBot_2.0.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from selenium import webdriver
 from pynput.keyboard import Key, Controller
+from csv import writer
 
 #CNN (Static website)
 def cnn():
@@ -20,12 +21,6 @@ def cnn():
     cnn_headlines = cnn_source.find_all('span', class_='cd__headline-text') 
     print("Scan complete.")
     return cnn_headlines
-
-# Loads website, and then saves html code from news website and searches through the code to match specific 
-# html chunk that returns necessary headline text only.
-
-## Note: this is a static website that does not change code through javascript, 
-# thus simplifying the scraping process.
 
 #Forbes (Static website)
 def forbes():
@@ -64,13 +59,6 @@ def bloomberg():
         print("Website html has changed, please update code")
     return bloomberg_headlines
 
-# Similar to Static websites, begins by loading the website. 
-# Since the website is dynamic, it includes bits of javascript coding that changes the underlying code,
-# we pauses our scraping for a few seconds for the page to completely load, and then proceed with the same
-# process of static websites.
-
-# Make sure geckodriver executable is in the PATH that way the website can be accessed through the firefox browser
-
 #MarketWatch (Dynamic website)
 def marketwatch():
     print("Scanning MarketWatch website...")
@@ -108,11 +96,6 @@ def wall_street_journal():
         print("Website html has changed, please update code")
     return WSJ_headlines
 
-# The code is working as of 11/15/22, future website maintenance may cause problems within the code, and require
-# debugging
-
-
-
 def scanning():
     cnn()
     forbes()
@@ -129,72 +112,79 @@ stock_list1.append("Amazon")
 stock_list1.append("Walmart")
 stock_list1.append("Google")
 stock_list1.append("Facebook")
-
-continue1 = input("Press any key to continue. ")
-print("\n")
+# add more words that would relate to tesla; ie spacex general tech, chips etc musk
+#continue1 = input("Press any key to continue. ")
+#print("\n")
 
 scanning()
 
 list_of_headlines = []
 
 def display_headlines():
+    now = datetime.now()
+    global date_time
+    date_time = now.strftime("%H:%M:%S %m/%d/%Y")
     for cnn_headline in cnn_headlines:
         for stonk in stock_list1:
             if stonk in cnn_headline.text:
                 #make a list to add the headlines, so that I can print them or add them to the csv
-                list_of_headlines.append(cnn_headline.text.replace(",",""))
+                list_of_headlines.append(cnn_headline.text.replace(",","")+""+","+date_time)
                 print(cnn_headline.text)
     for forbes_headline in forbes_headlines:
         for stonk2 in stock_list1: 
             if stonk2 in forbes_headline.text:
-                list_of_headlines.append(forbes_headline.text.replace(",",""))
+                list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+date_time)
                 print(forbes_headline.text)
     for bloomberg_headline in bloomberg_headlines:
         for stonk3 in stock_list1: 
             if stonk3 in bloomberg_headline.text:
-                list_of_headlines.append(bloomberg_headline.text.replace(",",""))
+                list_of_headlines.append(bloomberg_headline.text.replace(",","")+" "+","+date_time)
                 print(bloomberg_headline.text)
     for MW_headline in MW_headlines:
         for stonk4 in stock_list1: 
             if stonk4 in MW_headline.text:
-                list_of_headlines.append(forbes_headline.text.replace(",",""))
+                list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+date_time)
                 print(forbes_headline.text)
     for WSJ_headline in WSJ_headlines:
         for stonk5 in stock_list1: 
             if stonk5 in WSJ_headline.text:
-                list_of_headlines.append(WSJ_headline.text.replace(",",""))
+                list_of_headlines.append(WSJ_headline.text.replace(",","")+" "+","+date_time)
                 print(WSJ_headline.text)
 
 
-print("\n")
-print("HERE ARE TODAY'S HEADLINES:","\n")
-print("___________________________________________________________________________________")
-print("\n")
-display_headlines()
+#print("\n")
+#print("HERE ARE TODAY'S HEADLINES:","\n")
+#print("___________________________________________________________________________________")
+#print("\n")
+#display_headlines()
 
-print("New:")
-print(list_of_headlines)
-print('\n'.join([i for i in list_of_headlines[0:]]))
+#print("New:")
+#print(list_of_headlines)
+#print('\n'.join([i for i in list_of_headlines[0:]]))
 
+###
+with open('headline_logs.csv', 'a') as f_object:
+    writer_object = writer(f_object)
+    for i in list_of_headlines[0:]:
+        writer_object.writerow([i])
+    f_object.close()
 
-headline_logs = open('headline_logs.csv', 'w')
-headline_logs.write("HEADLINES:"+"\n")
-headline_logs.write('\n'.join([i for i in list_of_headlines[0:]]))
-headline_logs.close()
-
-
-
-def timestamp():
-    now = datetime.now()
-    global date_time
-    date_time = now.strftime("%H:%M:%S %m/%d/%Y")
-
-timestamp()
-headline_logs = open("headline_logs.csv", "a")
-headline_logs.write("\n")
-headline_logs.write("\n")
-headline_logs.write("Accessed: "+date_time)
-headline_logs.write("\n")
-headline_logs.close()
+##headline_logs = open('headline_logs.csv', 'w')
+#headline_logs.write("HEADLINES:"+"\n")
+##headline_logs.write('\n'.join([i for i in list_of_headlines[0:]]))
+##headline_logs.close()
 
 
+
+##def timestamp():
+##   now = datetime.now()
+##    global date_time
+##    date_time = now.strftime("%H:%M:%S %m/%d/%Y")
+
+#timestamp()
+#headline_logs = open("headline_logs.csv", "a")
+#headline_logs.write("\n")
+#headline_logs.write("\n")
+#headline_logs.write("Accessed: "+date_time)
+#headline_logs.write("\n")
+#headline_logs.close()
