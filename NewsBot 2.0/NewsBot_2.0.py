56a5d9e96c2984435e43fecbe9420d72ac903617
RawHeatEcon <kumar.rohitlove@yahoo.com>
2022-11-22 12:00:04 -0600

    Final additions
    Added tsla price ticker to output, added a delay to prevent scraper related ip blocks.

--- NewsBot 2.0/NewsBot_2.0.py
+++ NewsBot 2.0/NewsBot_2.0.py
@@ -15,29 +15,29 @@ from csv import writer
 def scrape_logger():
     #CNN (Static website)
     def cnn():
-        print("Scanning CNN website...")
+        #print("Scanning CNN website...")
         cnn_URL = 'https://www.cnn.com/business'
         cnn_page = requests.get(cnn_URL)
         cnn_raw_code = cnn_page.content
         cnn_source = BeautifulSoup(cnn_raw_code, "html.parser")
         global cnn_headlines
         cnn_headlines = cnn_source.find_all('span', class_='cd__headline-text') 
-        print("Scan complete.")
+        #print("Scan complete.")
         return cnn_headlines
     #Forbes (Static website)
     def forbes():
-        print("Scanning Forbes website...")
+        #print("Scanning Forbes website...")
         forbes_URL = 'https://www.forbes.com/'
         forbes_page = requests.get(forbes_URL)
         forbes_raw_code = forbes_page.content
         forbes_source = BeautifulSoup(forbes_raw_code, "html.parser")
         global forbes_headlines
         forbes_headlines = forbes_source.find_all('a', class_="happening__title")
-        print("Scan complete.")
+        #print("Scan complete.")
         return forbes_headlines
     #Bloomberg (Dynamic website)
     def bloomberg():
-        print("Scanning Bloomberg website...")
+        #print("Scanning Bloomberg website...")
         bloomberg_URL = 'https://www.bloomberg.com/markets/watchlist'
         bloomberg_page = webdriver.Firefox()
         bloomberg_page.get(bloomberg_URL)
@@ -54,25 +54,25 @@ def scrape_logger():
         bloomberg_source = BeautifulSoup(bloomberg_raw_code, "html.parser")
         global bloomberg_headlines
         bloomberg_headlines = bloomberg_source.find_all('a', class_="headline__55bd5397")    
-        print("Scan complete.")
+        #print("Scan complete.")
         if bloomberg_headlines == ():
             print("ERROR: \n")
             print("Website html has changed, please update code")
         return bloomberg_headlines
     #MarketWatch (Dynamic website)
     def marketwatch():
-        print("Scanning MarketWatch website...")
+        #print("Scanning MarketWatch website...")
         MW_URL = 'https://www.marketwatch.com/'
         MW_page = requests.get(MW_URL)
         MW_raw_code = MW_page.content
         MW_source = BeautifulSoup(MW_raw_code, "html.parser")
         global MW_headlines
         MW_headlines = MW_source.find_all('span', class_='headline')
-        print("Scan complete.")
+        #print("Scan complete.")
         return MW_headlines
     #WallStreetJournal (Static website)
     def wall_street_journal():
-        print("Scanning Wall Street Journal website...")
+        #print("Scanning Wall Street Journal website...")
         WSJ_URL = 'https://www.wsj.com/news/latest-headlines'
         WSJ_page = webdriver.Firefox()
         WSJ_page.get(WSJ_URL)
@@ -89,11 +89,23 @@ def scrape_logger():
         WSJ_source = BeautifulSoup(WSJ_raw_code, "html.parser")
         global WSJ_headlines
         WSJ_headlines = WSJ_source.find_all('div', class_='WSJTheme--headline--7VCzo7Ay')
-        print("Scan complete.")
+        #print("Scan complete.")
         if WSJ_headlines == ():
             print("ERROR: \n")
             print("Website html has changed, please update code")
         return WSJ_headlines
+
+    def tsla():
+        #print("Scanning tsla stock price...")
+        tsla_URL = 'https://www.cnbc.com/quotes/TSLA'
+        tsla_page = requests.get(tsla_URL)
+        tsla_raw_code = tsla_page.content
+        tsla_source = BeautifulSoup(tsla_raw_code, "html.parser")
+        global tsla_price
+        tsla_price = tsla_source.find('span', class_='QuoteStrip-lastPrice') 
+        #print("Scan complete.")
+        #print(tsla_price.text)
+        return tsla_price
 
     def scanning():
         cnn()
@@ -101,10 +113,10 @@ def scrape_logger():
         bloomberg()
         marketwatch()
         wall_street_journal()
+        tsla()
 
     global stock_list1
     stock_list1 = []
-
     stock_list1.append("Tesla")
     stock_list1.append("Elon")
     stock_list1.append("Musk")
@@ -150,28 +162,28 @@ def scrape_logger():
             for stonk in stock_list1:
                 if stonk in cnn_headline.text:
                     #make a list to add the headlines, so that I can print them or add them to the csv
-                    list_of_headlines.append(cnn_headline.text.replace(",","")+""+","+date_time)
-                    print(cnn_headline.text)
+                    list_of_headlines.append(cnn_headline.text.replace(",","")+" "+","+tsla_price.text+" "+","+date_time)
+                    #print(cnn_headline.text)
         for forbes_headline in forbes_headlines:
             for stonk2 in stock_list1: 
                 if stonk2 in forbes_headline.text:
-                    list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+date_time)
-                    print(forbes_headline.text)
+                    list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+tsla_price.text+" "+","+date_time)
+                    #print(forbes_headline.text)
         for bloomberg_headline in bloomberg_headlines:
             for stonk3 in stock_list1: 
                 if stonk3 in bloomberg_headline.text:
-                    list_of_headlines.append(bloomberg_headline.text.replace(",","")+" "+","+date_time)
-                    print(bloomberg_headline.text)
+                    list_of_headlines.append(bloomberg_headline.text.replace(",","")+" "+","+tsla_price.text+" "+","+date_time)
+                    #print(bloomberg_headline.text)
         for MW_headline in MW_headlines:
             for stonk4 in stock_list1: 
                 if stonk4 in MW_headline.text:
-                    list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+date_time)
-                    print(forbes_headline.text)
+                    list_of_headlines.append(forbes_headline.text.replace(",","")+" "+","+tsla_price.text+" "+","+date_time)
+                    #print(forbes_headline.text)
         for WSJ_headline in WSJ_headlines:
             for stonk5 in stock_list1: 
                 if stonk5 in WSJ_headline.text:
-                    list_of_headlines.append(WSJ_headline.text.replace(",","")+" "+","+date_time)
-                    print(WSJ_headline.text)
+                    list_of_headlines.append(WSJ_headline.text.replace(",","")+" "+","+tsla_price.text+" "+","+date_time)
+                    #print(WSJ_headline.text)
     display_headlines()
     with open('headline_logs.csv', 'a') as f_object:
         writer_object = writer(f_object)
@@ -184,5 +196,9 @@ def scrape_logger():
 try:
     while True:
         scrape_logger()
+        time.sleep(240)
 except KeyboardInterrupt:
     pass
+#Adding sleep delay to prevent being IP blocked for too many https requests
+
+
